cce76d5c2bc02f95391e04f47de6e506bd012a61
Joshua Steward <31291387+JoshuaSteward1193@users.noreply.github.com>
2021-02-22 15:15:07 -0600

    init

---
+++ MQ9/Assets/Scripts/CommandScreen/VitData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class contains the model of the vit data. It will need to map correctly to the JSON file we get from the current menu trainer
+/// </summary>
+public class VitData
+{
+    public int num;
+    public string title;
+    public string layout;
+    public string htmlStr;
+    public bool bind;
+    public VitField[] fields;
+}
---
+++ MQ9/Assets/Scripts/CommandScreen/VitField.cs
@@ -0,0 +1,9 @@
+﻿public class VitField
+{
+    public string varName;
+    public string title;
+    public string val;
+    public string units;
+    public float[] range;
+    public float[,] yellowsReds;
+}
