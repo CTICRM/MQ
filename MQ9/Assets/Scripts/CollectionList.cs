130c2c3016701035c3aa7f50fe6c88f2ccaf4017
tanner-mcdaniel-cti <78029717+tanner-mcdaniel-cti@users.noreply.github.com>
2021-03-01 16:27:39 -0600

    Map Updates, Popup Updates, and New Popup
    Users can now drag the map around smoothly, updated the design for some of the other popups, and started work on the popup for Ku SATCOM Configuration

--- MQ9/Assets/Scripts/CollectionList.cs
+++ MQ9/Assets/Scripts/CollectionList.cs
@@ -20,24 +20,61 @@ public class CollectionList : MonoBehaviour
 	public Text remarksData; */
 	public GameObject collectionListEntryPanel;
 	public GameObject collectionListEntryPrefab;
-	//int[] usedIDs;
-	float posY = -144.0f;
-
+	public GameObject errorIDPopup;
+	//GameObject newEntry;
+	List<GameObject> createdEntries = new List<GameObject>();
+	GameObject[] array;
+	List<string> usedIDs = new List<string>();
+	float posY = 0.0f;
+	
 	public void addEntry() {
-		GameObject newItemInList = Instantiate(collectionListEntryPrefab);
-		newItemInList.transform.parent = collectionListEntryPanel.transform;
-		newItemInList.transform.localPosition = new Vector3(0, posY, 0);
-		posY -= 100.0f;
-		string idText = id.text;
-		//Text typeText = "";
-		string hiddenText = "False";
-		string remarksText = remarks.text;
-		GameObject.Find("ID Data Text").GetComponent<Text>().text = idText;
-		//GameObject.Find("Type Data Text").GetComponent<Text>().text = "";
-		GameObject.Find("Hidden Data Text").GetComponent<Text>().text = hiddenText;
-		GameObject.Find("Remarks Data Text").GetComponent<Text>().text = remarksText;
-		//Instantiate(collectionListEntry, new Vector3(0, 
+		if (!usedIDs.Contains(id.text)) {
+			usedIDs.Add(id.text);
+			GameObject newEntry = Instantiate(collectionListEntryPrefab);
+			newEntry.transform.parent = collectionListEntryPanel.transform;
+			newEntry.transform.localPosition = new Vector3(0, posY, 0);
+			posY -= 70.0f;
+			GameObject.Find("ID Data Text").GetComponent<Text>().text = id.text;
+			/* collectionListEntryPrefab.SetParent(collectionListEntryPanel);
+			Instantiate(collectionListEntryPrefab, new Vector3(0, posY, 0), Quaternion.identity);
+			posY -= 100.0f;
+			collectionListEntryPrefab.Find("ID Data Text").GetComponent<Text>().text = id.text; */
+		}
+		else {
+			errorIDPopup.SetActive(true);
+		}
 	}
+	
+	/* public void addEntry() {
+		if (!usedIDs.Contains(id.text)) {
+			GameObject newItemInList = Instantiate(collectionListEntryPrefab);
+			newItemInList.transform.parent = collectionListEntryPanel.transform;
+			newItemInList.transform.localPosition = new Vector3(0, posY, 0);
+			posY -= 70.0f;
+			string idText = id.text;
+			Text typeText = "";
+			string hiddenText = "False";
+			string remarksText = remarks.text;
+			GameObject.Find("ID Data Text").GetComponent<Text>().text = idText;
+			GameObject.Find("Type Data Text").GetComponent<Text>().text = "";
+			GameObject.Find("Hidden Data Text").GetComponent<Text>().text = hiddenText;
+			GameObject.Find("Remarks Data Text").GetComponent<Text>().text = remarksText;
+			usedIDs.Add(idText);
+			array = GameObject.FindGameObjectsWithTag("Entry");
+			Destroy(newItemInList);
+			for (i = 0; i < array.Length; i++) {
+				createdEntries.Add(array[i]);
+			}
+			foreach (GameObject item in createdEntries) {
+				Instantiate(item);
+				item.transform.parent = collectionListEntryPanel.transform;
+				item.transform.localPosition = new Vector3(0, posY, 0);
+			}
+		}
+		else {
+			errorIDPopup.SetActive(true);
+		}
+	} */
     // Start is called before the first frame update
     void Start()
     {
