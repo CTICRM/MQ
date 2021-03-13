3ecc05d76891630c2aac61925b6cb6f7592b9b74
William Fulton <wfulton@cti-crm.com>
2021-03-12 23:48:12 -0600

    Updated

---
+++ MQ9/Assets/Scripts/H264AdvancedControls.cs
@@ -0,0 +1,266 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Houses all of the functions seen in the H.264 Adanced Controls popup
+
+public class H264AdvancedControls : MonoBehaviour
+{
+	public InputField streamPID;
+	public InputField minQuantI;
+	public InputField maxQuantI;
+	public InputField minQuantP;
+	public InputField maxQuantP;
+	public InputField ptsOffset;
+	public InputField gopSize;
+	public InputField sliceMacroblocks;
+	public InputField sliceBytes;
+	public InputField macroblocks;
+	public InputField intraRefreshPercent;
+	public InputField pid;
+	public InputField metaPID;
+	public InputField smptePID;
+	public InputField pcrPID;
+	public InputField clk;
+	public InputField pmtPID;
+	public InputField tsPacketsUDP;
+	public InputField timeToLive;
+	public InputField tosOctet;
+	public InputField localIPAddress;
+	public InputField localSubnetMask;
+	public InputField localGateway;
+	public InputField remoteIPAddress;
+	public InputField remotePort;
+	static int minQuantIValue = 5;
+	static int maxQuantIValue = 50;
+	static int minQuantPValue = 5;
+	static int maxQuantPValue = 50;
+	static int intraRefreshPercentValue = 3;
+	static int pidValue = 1001;
+	static int metaPIDValue = 1011;
+	static int smptePIDValue = 1012;
+	static int pcrPIDValue = 1000;
+	static int clkValue = 1776;
+	static int pmtPIDValue = 32;
+	static int tsPacketsUDPValue = 5;
+	static int timeToLiveValue = 4;
+	static int tosOctetValue = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        streamPID.text = "1000";
+		minQuantI.text = minQuantIValue.ToString();
+		maxQuantI.text = maxQuantIValue.ToString();
+		minQuantP.text = minQuantPValue.ToString();
+		maxQuantP.text = maxQuantPValue.ToString();
+		ptsOffset.text = "25000";
+		gopSize.text = "150";
+		sliceMacroblocks.text = "8195";
+		sliceBytes.text = "1400";
+		macroblocks.text = "360";
+		intraRefreshPercent.text = intraRefreshPercentValue.ToString();
+		pid.text = pidValue.ToString();
+		metaPID.text = metaPIDValue.ToString();
+		smptePID.text = smptePIDValue.ToString();
+		pcrPID.text = pcrPIDValue.ToString();
+		clk.text = clkValue.ToString();
+		pmtPID.text = pmtPIDValue.ToString();
+		tsPacketsUDP.text = tsPacketsUDPValue.ToString();
+		timeToLive.text = timeToLiveValue.ToString();
+		tosOctet.text = tosOctetValue.ToString();
+		localIPAddress.text = "10.10.0.10";
+		localSubnetMask.text = "255.0.0.0";
+		localGateway.text = "10.10.0.1";
+		remoteIPAddress.text = "10.10.0.11";
+		remotePort.text = "8000";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void minQuantIIncrement() {
+		minQuantIValue++;
+		minQuantI.text = minQuantIValue.ToString();
+	}
+	
+	public void minQuantIDecrement() {
+		minQuantIValue--;
+		if (minQuantIValue <= 0) {
+			minQuantIValue = 0;
+		}
+		minQuantI.text = minQuantIValue.ToString();
+	}
+	
+	public void maxQuantIIncrement() {
+		maxQuantIValue++;
+		maxQuantI.text = maxQuantIValue.ToString();
+	}
+	
+	public void maxQuantIDecrement() {
+		maxQuantIValue--;
+		if (maxQuantIValue <= 0) {
+			maxQuantIValue = 0;
+		}
+		maxQuantI.text = maxQuantIValue.ToString();
+	}
+	
+	public void minQuantPIncrement() {
+		minQuantPValue++;
+		minQuantP.text = minQuantPValue.ToString();
+	}
+	
+	public void minQuantPDecrement() {
+		minQuantPValue--;
+		if (minQuantPValue <= 0) {
+			minQuantPValue = 0;
+		}
+		minQuantP.text = minQuantPValue.ToString();
+	}
+	
+	public void maxQuantPIncrement() {
+		maxQuantPValue++;
+		maxQuantP.text = maxQuantPValue.ToString();
+	}
+	
+	public void maxQuantPDecrement() {
+		maxQuantPValue--;
+		if (maxQuantPValue <= 0) {
+			maxQuantPValue = 0;
+		}
+		maxQuantP.text = maxQuantPValue.ToString();
+	}
+	
+	public void intraRefreshPercentIncrement() {
+		intraRefreshPercentValue++;
+		intraRefreshPercent.text = intraRefreshPercentValue.ToString();
+	}
+	
+	public void intraRefreshPercentDecrement() {
+		intraRefreshPercentValue--;
+		if (intraRefreshPercentValue <= 0) {
+			intraRefreshPercentValue = 0;
+		}
+		intraRefreshPercent.text = intraRefreshPercentValue.ToString();
+	}
+	
+	public void pidIncrement() {
+		pidValue++;
+		pid.text = pidValue.ToString();
+	}
+	
+	public void pidDecrement() {
+		pidValue--;
+		if (pidValue <= 0) {
+			pidValue = 0;
+		}
+		pid.text = pidValue.ToString();
+	}
+	
+	public void metaPIDIncrement() {
+		metaPIDValue++;
+		metaPID.text = metaPIDValue.ToString();
+	}
+	
+	public void metaPIDDecrement() {
+		metaPIDValue--;
+		if (metaPIDValue <= 0) {
+			metaPIDValue = 0;
+		}
+		metaPID.text = metaPIDValue.ToString();
+	}
+	
+	public void smptePIDIncrement() {
+		smptePIDValue++;
+		smptePID.text = smptePIDValue.ToString();
+	}
+	
+	public void smptePIDDecrement() {
+		smptePIDValue--;
+		if (smptePIDValue <= 0) {
+			smptePIDValue = 0;
+		}
+		smptePID.text = smptePIDValue.ToString();
+	}
+	
+	public void pcrPIDIncrement() {
+		pcrPIDValue++;
+		pcrPID.text = pcrPIDValue.ToString();
+	}
+	
+	public void pcrPIDDecrement() {
+		pcrPIDValue--;
+		if (pcrPIDValue <= 0) {
+			pcrPIDValue = 0;
+		}
+		pcrPID.text = pcrPIDValue.ToString();
+	}
+	
+	public void clkIncrement() {
+		clkValue++;
+		clk.text = clkValue.ToString();
+	}
+	
+	public void clkDecrement() {
+		clkValue--;
+		if (clkValue <= 0) {
+			clkValue = 0;
+		}
+		clk.text = clkValue.ToString();
+	}
+	
+	public void pmtPIDIncrement() {
+		pmtPIDValue++;
+		pmtPID.text = pmtPIDValue.ToString();
+	}
+	
+	public void pmtPIDDecrement() {
+		pmtPIDValue--;
+		if (pmtPIDValue <= 0) {
+			pmtPIDValue = 0;
+		}
+		pmtPID.text = pmtPIDValue.ToString();
+	}
+	
+	public void tsPacketsUDPIncrement() {
+		tsPacketsUDPValue++;
+		tsPacketsUDP.text = tsPacketsUDPValue.ToString();
+	}
+	
+	public void tsPacketsUDPDecrement() {
+		tsPacketsUDPValue--;
+		if (tsPacketsUDPValue <= 0) {
+			tsPacketsUDPValue = 0;
+		}
+		tsPacketsUDP.text = tsPacketsUDPValue.ToString();
+	}
+	
+	public void timeToLiveIncrement() {
+		timeToLiveValue++;
+		timeToLive.text = timeToLiveValue.ToString();
+	}
+	
+	public void timeToLiveDecrement() {
+		timeToLiveValue--;
+		if (timeToLiveValue <= 0) {
+			timeToLiveValue = 0;
+		}
+		timeToLive.text = timeToLiveValue.ToString();
+	}
+	
+	public void tosOctetIncrement() {
+		tosOctetValue++;
+		tosOctet.text = tosOctetValue.ToString();
+	}
+	
+	public void tosOctetDecrement() {
+		tosOctetValue--;
+		if (tosOctetValue <= 0) {
+			tosOctetValue = 0;
+		}
+		tosOctet.text = tosOctetValue.ToString();
+	}
+}
---
+++ MQ9/Assets/Scripts/SatelliteA.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Houses the functions for Satellite A found in the Ku SATCOM
+// Configuration popup
+
+public class SatelliteA : MonoBehaviour
+{
+	public InputField satACLFrequency;
+	public InputField satARLFrequency;
+	public Button satASpread;
+	public Button satAPolar;
+	public InputField satAPNCode;
+    public Button satATwoLineNORAD;
+	public Button satAIntelSAT;
+	public InputField satASATInput;
+	/* public Button currJanA;
+	public Button currFebA;
+	public Button currMarA;
+	public Button currAprA;
+	public Button currMayA;
+	public Button currJunA;
+	public Button currJulA;
+	public Button currAugA;
+	public Button currSepA;
+	public Button currOctA;
+	public Button currNovA;
+	public Button currDecA;
+	public InputField currDayA;
+	public InputField currYearA;
+	public InputField currHourA;
+	public InputField currMinA;
+	public InputField currSecA;
+	public Button epochJanA;
+	public Button epochFebA;
+	public Button epochMarA;
+	public Button epochAprA;
+	public Button epochMayA;
+	public Button epochJunA;
+	public Button epochJulA;
+	public Button epochAugA;
+	public Button epochSepA;
+	public Button epochOctA;
+	public Button epochNovA;
+	public Button epochDecA;
+	public InputField epochDayA;
+	public InputField epochYearA;
+	public InputField epochHourA;
+	public InputField epochMinA;
+	public InputField epochSecA; */
+	public InputField satALONC;
+	public InputField satALATC;
+	public InputField satALONC1;
+	public InputField satALATC1;
+	public InputField satALONS;
+	public InputField satALATS;
+	public InputField satALONS1;
+	public InputField satALATS1;
+	public InputField satA170HourLongitude;
+	public InputField satA170HourLatitude;
+	public InputField satALM0;
+	public InputField satALM1;
+	public InputField satALM2;
+	public InputField satANORADTwoLineElementEphemerisData1;
+	public InputField satANORADTwoLineElementEphemerisData2;
+	static float satACLFrequencyValue = 12000.250f;
+	static float satARLFrequencyValue = 14000.250f;
+	static int satAPNCodeValue = 0;
+	Color32 onBlack = new Color32(0, 0, 0, 255); // The button is selected
+	Color32 offWhite = new Color32(255, 255, 255, 255); // The button is no longer selected
+    // Start is called before the first frame update
+    void Start()
+    {
+        satALONC.text = "0.000000";
+		satALONC1.text = "0.000000";
+		satALONS.text = "0.000000";
+		satALONS1.text = "0.000000";
+		satALATC.text = "0.000000";
+		satALATC1.text = "0.000000";
+		satALATS.text = "0.000000";
+		satALATS1.text = "0.000000";
+		satA170HourLongitude.text = "0.000000";
+		satA170HourLatitude.text = "0.000000";
+		satALM0.text = "0.000000";
+		satALM1.text = "0.000000";
+		satALM2.text = "0.000000";
+		satACLFrequency.text = satACLFrequencyValue.ToString();
+		satARLFrequency.text = satARLFrequencyValue.ToString();
+		satAPNCode.text = satAPNCodeValue.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+		
+	public void satATwoLineNORADClick() {
+		satATwoLineNORAD.GetComponent<Image>().color = onBlack;
+		satAIntelSAT.GetComponent<Image>().color = offWhite;
+	}
+	
+	public void satAIntelSATClick() {
+		satATwoLineNORAD.GetComponent<Image>().color = offWhite;
+		satAIntelSAT.GetComponent<Image>().color = onBlack;
+	}
+	
+	public void satACLFrequencyIncrement() {
+		satACLFrequencyValue += 0.001f;
+		satACLFrequency.text = satACLFrequencyValue.ToString();
+	}
+	
+	public void satACLFrequencyDecrement() {
+		satACLFrequencyValue -= 0.001f;
+		if (satACLFrequencyValue <= 0) {
+			satACLFrequencyValue = 0;
+		}
+		satACLFrequency.text = satACLFrequencyValue.ToString();
+	}
+	
+	public void satARLFrequencyIncrement() {
+		satARLFrequencyValue += 0.001f;
+		satARLFrequency.text = satARLFrequencyValue.ToString();
+	}
+	
+	public void satARLFrequencyDecrement() {
+		satARLFrequencyValue -= 0.001f;
+		if (satARLFrequencyValue <= 0) {
+			satARLFrequencyValue = 0;
+		}
+		satARLFrequency.text = satARLFrequencyValue.ToString();
+	}
+	
+	public void satASpreadClick() {
+		if (satASpread.GetComponentInChildren<Text>().text == "Disabled") {
+			satASpread.GetComponentInChildren<Text>().text = "Enabled";
+		}
+		else {
+			satASpread.GetComponentInChildren<Text>().text = "Disabled";
+		}
+	}
+	
+	public void satAPolarClick() {
+		if (satAPolar.GetComponentInChildren<Text>().text == "Vertical") {
+			satAPolar.GetComponentInChildren<Text>().text = "Horizontal";
+		}
+		else {
+			satAPolar.GetComponentInChildren<Text>().text = "Vertical";
+		}
+	}
+	
+	public void satAPNCodeIncrement() {
+		satAPNCodeValue++;
+		satAPNCode.text = satAPNCodeValue.ToString();
+	}
+	
+	public void satAPNCodeDecrement() {
+		satAPNCodeValue--;
+		if (satAPNCodeValue <= 0) {
+			satAPNCodeValue = 0;
+		}
+		satAPNCode.text = satAPNCodeValue.ToString();
+	}
+}
